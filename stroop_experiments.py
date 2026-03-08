7b368b6557d091df469121069ca7fa48ba677b47
Piper Hanrahan <piper.kacy@gmail.com>
2026-03-08 11:01:56 -0400

    Commit 6 - func stubs

---
+++ stroop_experiments.py
@@ -0,0 +1,17 @@
+def determine_condition(word, color):
+    pass
+
+def is_correct_response(response, color):
+    pass
+
+def generate_trial():
+    pass
+
+def record_trial_results(participant, word, color, condition, response, reaction_time):
+    pass
+
+def save_results_to_json(results, filepath):
+    pass
+
+def calculate_average_reaction_time(results):
+    pass
